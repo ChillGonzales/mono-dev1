fff81ef7d75d89e5c4c280e7ed97ca44273dcf72
Vigenere <cmmonroe2010@gmail.com>
2016-05-30 11:28:50 -0400

    Added circle collider class
    Circle collider class for collisions.

---
+++ Game1/Physics/CircleCollider2D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Game1.Physics
+{
+    public struct CircleCollider2D
+    {
+        public Vector2 Center { get; set; }
+        public float Radius { get; set; }
+
+        public CircleCollider2D(Vector2 center, float radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public bool Contains(Vector2 point)
+        {
+            return ((point - Center).Length() <= Radius);
+        }
+
+        public bool Intersects(CircleCollider2D other)
+        {
+            return ((other.Center)-Center).Length() < (other.Radius - Radius));
+        }
+    }
+}
